7304d59bd55428b5c30131ac36af0ea89fcd76c2
DaianaJS <daianasotelo18@gmail.com>
2022-11-27 23:31:48 -0300

    Creación de elementos para Maqueta Dinámica y componentes.

---
+++ MaquetaDinamica/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercademiComponent } from './components/acercademi/acercademi.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HardskillsComponent } from './components/hardskills/hardskills.component';
+import { SoftskillsComponent } from './components/softskills/softskills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent,
+    AcercademiComponent,
+    EducacionComponent,
+    NavbarComponent,
+    ExperienciaComponent,
+    HardskillsComponent,
+    SoftskillsComponent,
+    ProyectosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
